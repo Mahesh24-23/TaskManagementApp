d016db721ec04e187750ad1b89a43cbf5a3feb4f
Mahesh24-23 <maheshmb244@gmail.com>
2022-02-26 10:57:00 +0530

    edit card

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ localStorage.setItem("taskyCA", JSON.stringify({ cards: globalTaskData }));
 const newCard=`<div class="col-md-6 col-lg-4 my-4" id=${card.id}>
 <div class="card">
   <div class="card-header d-flex justify-content-end gap-2">
-    <button class="btn btn-outline-info">
-      <i class="fa fa-pencil" aria-hidden="true"></i>
+    <button class="btn btn-outline-info" name="${card.id}" onclick="editCard.apply(this,arguments)">
+      <i class="fa fa-pencil" aria-hidden="true" name="${card.id}" ></i>
     </button>
    <button class="btn btn-outline-danger" name="${card.id}" onclick="deleteCard.apply(this,arguments)">
      <i class="fa fa-trash-o" aria-hidden="true" name="${card.id}" onclick="deleteCard.apply(this,arguments)"></i>
@@ -77,8 +77,8 @@ globalTaskData.map((card)=>{
   newCard= `<div class="col-md-6 col-lg-4 my-4" id=${card.id} >
   <div class="card">
     <div class="card-header d-flex justify-content-end gap-2">
-      <button class="btn btn-outline-info">
-        <i class="fa fa-pencil" aria-hidden="true"></i>
+      <button class="btn btn-outline-info" name="${card.id}" onclick="editCard.apply(this,arguments)">
+        <i class="fa fa-pencil" aria-hidden="true" name="${card.id}"></i>
       </button>
      <button class="btn btn-outline-danger" name="${card.id}" onclick="deleteCard.apply(this,arguments)">
        <i class="fa fa-trash-o" aria-hidden="true" name="${card.id}" onclick="deleteCard.apply(this,arguments)"></i>
@@ -94,7 +94,7 @@ globalTaskData.map((card)=>{
       </p>
     </div>
     <div class="card-footer">
-      <button class="btn btn-outline-primary">View Task</button>
+      <button class="btn btn-outline-primary" name="${card.id}">View Task</button>
     </div>
   </div>
   
@@ -126,12 +126,74 @@ const deleteCard=(event)=>{
   else {
     return taskContainer.removeChild(event.target.parentNode.parentNode.parentNode.parentNode);
   }
+};
+
+
+const editCard=(event)=>{
+  const targetID=event.target.getAttribute("name");
+  const elementType=event.target.tagName;
+
+  let taskTitle;
+  let TaskType;
+  let taskDescription;
+  let parentElement;
+  let submitButton;
+  if(elementType==="BUTTON"){
+    parentElement=event.target.parentNode.parentNode.parentNode;
+
+  }
+
+  else{
+    parentElement=event.target.parentNode.parentNode.parentElement.parentNode;
+  }
+  taskTitle=parentElement.childNodes[1].childNodes[3].childNodes[3];
+  taskDescription=parentElement.childNodes[1].childNodes[3].childNodes[5];
+  submitButton=parentElement.childNodes[1].childNodes[5].childNodes[1];
+  taskTitle.setAttribute("contenteditable","true");
+  taskDescription.setAttribute("contenteditable","true");
+  submitButton.innerHTML="Save Changes";
+  submitButton.setAttribute("onclick","saveEdit.apply(this,arguments)");
+};
 
 
 
+const saveEdit=(event)=>{
+  const targetID=event.target.getAttribute("name");
+  const elementType=event.target.tagName;
 
+  let taskTitle;
+  let TaskType;
+  let taskDescription;
+  let parentElement;
+  let submitButton;
+  if(elementType==="BUTTON"){
+    parentElement=event.target.parentNode.parentNode.parentNode;
 
+  }
 
+  else{
+    parentElement=event.target.parentNode.parentNode.parentElement.parentNode;
+  }
+  taskTitle=parentElement.childNodes[1].childNodes[3].childNodes[3];
+  taskDescription=parentElement.childNodes[1].childNodes[3].childNodes[5];
+  submitButton=parentElement.childNodes[1].childNodes[5].childNodes[1];
 
+  const updatedData={
+    title:taskTitle.innerHTML,
+    description:taskDescription.innerHTML,
+  };
 
+  globalTaskData.forEach((task)=>{
+    if(task.id===targetID){
+      return {...task,...updatedData};
+    }
+    else{
+      return task;
+    }
+  })
+
+  localStorage.setItem("taskyCA", JSON.stringify({ cards: globalTaskData }));
+  taskTitle.setAttribute("contenteditable","false");
+  taskDescription.setAttribute("contenteditable","false");
+  submitButton.innerHTML="View Task";
 };
